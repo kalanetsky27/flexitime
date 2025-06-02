6950e3f4c3d65b6e3742da47dd9e1c5c6f3d6547
kalanetsky27 <113824323+kalanetsky27@users.noreply.github.com>
2025-06-02 20:55:03 +0300

    Add files via upload

---
+++ cabinet/js/bumpix/locales/language_locales.js
@@ -0,0 +1,10 @@
+(function($) {
+    $.l_sets = {
+        locale_items: [
+            {id: "ru", name: "Русский"},
+            {id: "uk", name: "Українська"},
+            {id: "en", name: "English"}
+        ]
+    };
+})(jQuery);
+
